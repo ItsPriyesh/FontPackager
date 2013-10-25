9fa7ce0387f60b6cf22ea6243b08dc1f8af4464b
Priyesh Patel <priyesh.96@hotmail.com>
2013-10-25 18:07:28 -0400

    Push everything to github

---
+++ Chromium's_Font_Packager.bat
@@ -0,0 +1,152 @@
+@echo off
+title Chromium's Font Packager
+mode con:cols=100 lines=50
+color 0b
+:: x variable default value of 0. For each of the 4 base fonts: if base font exists and the derivatives are created, update the value of x by adding 1. Therefore if x=4 at the end, all fonts have been created successfully.
+set x=0
+
+echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+echo "^                    ____ _                         _                 _                         ^"
+echo "^                   / ___| |__  _ __ ___  _ __ ___ (_)_   _ _ __ ___ ( )___                     ^"
+echo "^                  | |   | '_ \| '__/ _ \| '_ ` _ \| | | | | '_ ` _ \|// __|                    ^"
+echo "^                  | |___| | | | | | (_) | | | | | | | |_| | | | | | | \__ \                    ^"
+echo "^                 __\____|_| |_|_|_ \___/|_| |_| |_|_|\__,_|_| |_| |_| |___/                    ^"
+echo "^                |  ___|__  _ __ | |_  |  _ \ __ _  ___| | ____ _  __ _  ___ _ __               ^"
+echo "^                | |_ / _ \| '_ \| __| | |_) / _` |/ __| |/ / _` |/ _` |/ _ \ '__|              ^"
+echo "^                |  _| (_) | | | | |_  |  __/ (_| | (__|   < (_| | (_| |  __/ |                 ^"
+echo "^                |_|  \___/|_| |_|\__| |_|   \__,_|\___|_|\_\__,_|\__, |\___|_|                 ^"
+echo "^                                                                 |___/                         ^"
+echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+echo.
+
+::Instructions
+echo Requirements/Instructions: 
+echo.
+echo Put the following 4 following files inside of the Input folder:
+echo 	1. Roboto-Regular.ttf
+echo 	2. Roboto-Italic.ttf
+echo 	3. Roboto-Bold.ttf
+echo 	4. Roboto-BoldItalic.tff
+echo.
+echo Once you have added the above files into the appropriate folder, press any key to begin . . .
+pause >nul
+
+::Get font name
+echo.
+set /p fontname=Please enter the name of the font without spaces: 
+
+::Roboto-Regular derivatives
+if exist Input\Roboto-Regular.ttf (
+copy /y Input\Roboto-Regular.ttf RobotoCondensed-Regular.ttf >nul
+copy /y Input\Roboto-Regular.ttf Roboto-Light.ttf >nul
+copy /y Input\Roboto-Regular.ttf Roboto-Thin.ttf >nul
+set /a x=1
+)
+
+::Roboto-Italic derivatives
+if exist Input\Roboto-Italic.ttf (
+copy /y Input\Roboto-Italic.ttf RobotoCondensed-Italic.ttf >nul
+copy /y Input\Roboto-Italic.ttf Roboto-LightItalic.ttf >nul
+copy /y Input\Roboto-Italic.ttf Roboto-ThinItalic.ttf >nul
+set /a x=x+1
+)
+
+::Roboto-Bold derivatives
+if exist Input\Roboto-Bold.ttf (
+copy /y Input\Roboto-Bold.ttf RobotoCondensed-Bold.ttf >nul
+set /a x=x+1
+)
+
+::Roboto-BoldItalic derivatives
+if exist Input\Roboto-BoldItalic.ttf (
+copy /y Input\Roboto-BoldItalic.ttf RobotoCondensed-BoldItalic.ttf >nul
+set /a x=x+1
+)
+
+::Check to see if all fonts were created successfully
+if %x% EQU 4 (
+echo.
+echo %fontname% Font pack created successfully! It can be found in the Output folder.
+echo.
+)
+
+::Create new directory and move fonts into it
+mkdir fonts
+move Input\Roboto-Regular.ttf fonts >nul
+move Input\Roboto-Italic.ttf fonts >nul
+move Input\Roboto-Bold.ttf fonts >nul
+move Input\Roboto-BoldItalic.ttf fonts >nul
+move Roboto-Light.ttf fonts >nul
+move Roboto-LightItalic.ttf fonts >nul
+move Roboto-Thin.ttf fonts >nul
+move Roboto-ThinItalic.ttf fonts >nul
+move RobotoCondensed-Regular.ttf fonts >nul
+move RobotoCondensed-Italic.ttf fonts >nul
+move RobotoCondensed-Bold.ttf fonts >nul
+move RobotoCondensed-BoldItalic.ttf fonts >nul
+
+::Create META-INF directory with subfolders
+mkdir META-INF
+cd META-INF
+mkdir com
+cd com
+mkdir google
+cd google
+mkdir android
+cd android
+
+::Create updater-script
+cd.>updater-script
+echo ui_print("%fontname% - Font Pack");>updater-script
+echo ui_print("Created using Chromium's Font Packager");>>updater-script
+echo run_program("/sbin/busybox", "mount", "/system");>>updater-script
+echo package_extract_dir("fonts", "/system/fonts");>>updater-script
+echo run_program("/sbin/busybox", "umount", "/system");>>updater-script
+echo ui_print("Font installation complete!");>>updater-script
+
+::Create update-binary
+::cd.>update-binary
+cd..
+cd..
+cd..
+cd..
+::Back to root
+copy CoreFiles\update-binary META-INF\com\google\android >nul
+
+::Convert EOL from Windows to Unix (this prevents status 6 error when flashing)
+CoreFiles\dos2unix META-INF\com\google\android\updater-script >nul
+
+::Zip it all up
+CoreFiles\7za a %fontname%FontPack.zip fonts META-INF >nul
+
+::Move zip to out
+move %fontname%FontPack.zip Output >nul
+::Delete remnants
+rmdir /s /q fonts
+rmdir /s /q META-INF
+
+echo.
+set /p copytophone=Would you like the %fontname%FontPack.zip to be copied to your phone? [y or n]: 
+if "%copytophone%"=="y" goto yes
+if "%copytophone%"=="n" goto no
+
+:yes
+adb shell mkdir /sdcard/FontPacks >nul
+adb push Output\%fontname%FontPack.zip /sdcard/FontPacks/
+echo.
+set /p rebootrecovery=Would you like your phone to be rebooted into recovery? [y or n]: 
+if "%rebootrecovery%"=="y" goto yes2
+if "%rebootrecovery%"=="n" goto no
+
+:yes2
+adb reboot recovery >nul
+
+:no
+::Exit
+echo.
+echo Press any key to exit . . .
+pause >nul
+
+
+
+
